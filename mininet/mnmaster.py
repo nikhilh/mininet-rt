4152c6cd18d27fa52af91b2a2d802e64bbae1672
Nikhil Handigol <nikhilh@stanford.edu>
2010-08-25 01:43:31 -0700

    master and slave scripts with minimal rpc protocol - bw isolation benchmark now works remotely

--- mininet/mnmaster.py
+++ mininet/mnmaster.py
@@ -5,13 +5,68 @@ from time import sleep
 import xmlrpclib
 from xmlrpclib import ServerProxy, Binary
 from cPickle import dumps, loads
-from mininet.topo import SingleSwitchTopo
+from mininet.topo import Topo, SingleSwitchTopo
+
+serverIP = '127.0.0.1'
+serverPort = 8000
+server = None
+
+class MininetMasterService:
+    '''A mininet proxy to the slaves'''
+
+    def mininetStart(self, topo, cparam):
+        '''Start mininet with the given topo and ctrlr params as args'''
+        global server, serverIP, serverPort
+        t = Topo()
+        t.g = topo.g
+        t.node_info = topo.node_info
+        t.edge_info = topo.edge_info
+        t.ports = topo.ports
+        t.id_gen = topo.id_gen
+        server = ServerProxy('http://%s:%d/' % (serverIP, serverPort), allow_none=True)
+        try:
+            server.mininetStart(Binary(dumps(t)), Binary(dumps(cparam)))
+        except xmlrpclib.Fault as e:
+            print e
+            exit(1)
+
+    def mininetStop(self):
+        server.mininetStop()
+
+    def mininetGetHosts(self):
+        phosts = server.mininetGetHosts()
+        hosts = loads(phosts.data)
+        return hosts
+
+    def mininetGetSwitches(self):
+        pswitches = server.mininetGetSwitches()
+        switches = loads(pswitches.data)
+        return switches
+
+    def getHostIP(self, hostName, intf=None):
+        return server.getHostIP(hostName, intf)
+
+    def getCmd(self, hostName, cmd):
+        return server.getCmd(cmd)
+
+    def cmdSend(self, hostName, cmd):
+        return server.cmdSend(hostName, cmd)
+
+    def cmdReadLine(self, hostName, handle):
+        return server.cmdReadLine(handle)
+
+    def cmdWaitOutput(self, hostName, handle):
+        return server.cmdWaitOutput(handle)
+
+    def cmdKill(self, hostName, handle):
+        return server.cmdKill(handle)
+
+    def cmdWait(self, hostName, handle):
+        return server.cmdWait(handle)
+
 
 if __name__ == '__main__':
-    serverIP = '127.0.0.1'
-    serverPort = 8000
     size = 2
-
     if(len(sys.argv) > 1):
         serverIP = sys.argv[1]
     if(len(sys.argv) > 2):
@@ -19,9 +74,6 @@ if __name__ == '__main__':
             serverPort = int(sys.argv[2])
         except ValueError:
             print 'Could not convert arg %s to int - using default value' % sys.argv[2]
-
-    server = ServerProxy('http://%s:%d/' % (serverIP, serverPort))
-
 
     #try starting the network
     cparam = ('127.0.0.1', 6633)
--- mininet/mnslave.py
+++ mininet/mnslave.py
@@ -6,7 +6,7 @@ from SimpleXMLRPCServer import SimpleXMLRPCServer
 from xmlrpclib import Binary
 from cPickle import dumps, loads
 
-from mininet.net import Mininet
+from mininet.net import Mininet, init
 from mininet.node import RemoteController, OVSKernelSwitch
 from mininet.clean import cleanup
 import mininet.util as util
@@ -22,9 +22,9 @@ class MininetSlaveService:
         cparams = loads(pcparams.data)
         topo = loads(ptopo.data)
         c = lambda a: RemoteController(a, defaultIP=cparams[0], port=cparams[1])
+        init()
         net = Mininet(topo, switch=OVSKernelSwitch, controller=c, autoSetMacs=True, autoStaticArp=False)
         net.start()
-        print net.nameToNode.keys()
         return
 
     def mininetStop(self):
@@ -43,6 +43,22 @@ class MininetSlaveService:
             return Binary(dumps(None))
         return Binary(dumps([h.name for h in net.hosts]))
 
+    def mininetGetSwitches(self):
+        '''Returns a pickled list of host names'''
+        global net
+        if(net is None):
+            return Binary(dumps(None))
+        return Binary(dumps([s.name for s in net.switches]))
+
+    def getHostIP(self, hostName, intf=None):
+        global net
+        if(net is None):
+            return None
+        host = net.nameToNode[hostName]
+        if(host is None):
+            return None
+        return host.IP(intf=intf)
+
     def getCmd(self, cmd):
         return util.getCmd(cmd)
 
@@ -59,31 +75,31 @@ class MininetSlaveService:
         return handle
 
     def cmdReadLine(self, handle):
-        c = cmdDict[handle]
-        if(c is None):
+        if(handle not in cmdDict):
             return None
+        c = cmdDict[handle]
         return c.readLine()
 
     def cmdWaitOutput(self, handle):
-        c = cmdDict[handle]
-        if(c is None):
+        if(handle not in cmdDict):
             return None
+        c = cmdDict[handle]
         ret = c.waitOutput()
         del cmdDict[handle]
         return ret
 
     def cmdKill(self, handle):
-        c = cmdDict[handle]
-        if(c is None):
+        if(handle not in cmdDict):
             return None
+        c = cmdDict[handle]
         ret = c.kill()
         del cmdDict[handle]
         return ret
 
     def cmdWait(self, handle):
-        c = cmdDict[handle]
-        if(c is None):
+        if(handle not in cmdDict):
             return None
+        c = cmdDict[handle]
         ret = c.wait()
         del cmdDict[handle]
         return ret
@@ -97,7 +113,7 @@ if __name__ == '__main__':
             port = int(sys.argv[1])
         except ValueError:
             print 'Could not convert the argument "%s" to int' % sys.argv[1]
-    server = SimpleXMLRPCServer(("localhost", port), allow_none=True)
+    server = SimpleXMLRPCServer(("localhost", port), logRequests=False, allow_none=True)
     print "Listening on port 8000..."
     # Register functions
     server.register_instance(MininetSlaveService())
